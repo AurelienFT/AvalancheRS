4695195a0564b1f5bbd3e09071595cc101fb2067
AurelienFT <aurelien.foucault@epitech.eu>
2021-12-16 23:43:08 +0100

    [ADD] Method to call api.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ A Avalanche Platform Rust Library
 
 Inspired from [AvalancheJS](https://github.com/ava-labs/avalanchejs) to allow you to interact with a node in Rust.
 
+It's my first library so if you have any design proposal or suggestions don't hesitate to open an issue.
+
 This library is still a WIP.
--- src/avalanche.rs
+++ src/avalanche.rs
@@ -1,30 +1,96 @@
-use hyper::{Client, Uri};
+use hyper::{Client, Request, Method, Body};
+use url::Url;
+use hyper::client::{ResponseFuture};
+use hyper::header::{HeaderValue, HeaderName};
 use crate::errors::{AvalancheError};
+use crate::utils::constants::{FALLBACK_HRP, NETWORK_ID_TO_HRP, DEFAULT_NETWORK_ID};
 use regex::Regex;
+use std::collections::HashMap;
 use std::str::FromStr;
 
 #[derive(Debug, Clone, Default)]
 struct Avalanche {
-    network_id: i32,
+    network_id: u16,
     hrp: String,
     protocol: String,
     host: String,
     ip: String,
-    port: i32,
-    url: String
+    port: u32,
+    url: String,
+    headers: HashMap<String, String>,
+    auth: Option<String>
 }
 
 trait AvalancheCore {
     fn set_address(&mut self, host: String, port: u32, protocol: Option<&str>) -> Result<(), AvalancheError>;
+    fn get_protocol(&self) -> &str;
+    fn get_host(&self) -> &str;
+    fn get_ip(&self) -> &str;
+    fn get_port(&self) -> u32;
+    fn get_url(&self) -> &str;
+    fn get_headers(&self) -> &HashMap<String, String>;
+    fn get_network_id(&self) -> u16;
+    fn set_network_id(&mut self, network_id: u16);
+    fn get_hrp(&self) -> &str;
+    fn set_hrp(&mut self, hrp: &str);
+    fn set_header(&mut self, key: &str, value: &str);
+    fn remove_header(&mut self, key: &str);
+    fn remove_all_headers(&mut self);
+    fn set_auth_token(&mut self, token: &str);
+    fn get(&self, url: &str, get_data: HashMap<&str, &str>, headers: HashMap<&str, &str>) -> ResponseFuture;
+    fn delete(&self, url: &str, get_data: HashMap<&str, &str>, headers: HashMap<&str, &str>) -> ResponseFuture;
+    fn post(&self, url: &str, post_data: Body, headers: HashMap<&str, &str>) -> ResponseFuture;
+    fn put(&self, url: &str, post_data: Body, headers: HashMap<&str, &str>) -> ResponseFuture;
+    fn patch(&self, url: &str, post_data: Body, headers: HashMap<&str, &str>) -> ResponseFuture;
 }
 
 impl Avalanche {
-  fn new(host: String, port: u32, protocol: Option<&str>) -> Result<Avalanche, AvalancheError> {
+  pub fn new(host: String, port: u32, protocol: Option<&str>) -> Result<Avalanche, AvalancheError> {
     let mut avalanche = Avalanche::default();
     match avalanche.set_address(host, port, protocol) {
       Ok(_) => Ok(avalanche),
       Err(e) => Err(e)
     }
+  }
+  fn set_header(&self, mut request: hyper::Request<hyper::Body>, headers: HashMap<&str, &str>) -> hyper::Request<hyper::Body>{
+    for (key, value) in headers {
+      request.headers_mut().insert(HeaderName::from_str(key).unwrap(), HeaderValue::from_str(value).unwrap());
+    }
+    for (key, value) in self.headers.iter() {
+      request.headers_mut().insert(HeaderName::from_str(key).unwrap(), HeaderValue::from_str(value).unwrap());
+    }
+    match self.auth {
+      Some(ref token) => {
+        request.headers_mut().insert(HeaderName::from_str("Authorization").unwrap(), HeaderValue::from_str(format!("Bearer {}", token)).unwrap());
+      },
+      None => {}
+    }
+    request
+  }
+  fn request(&self, url: &str, method: Method, get_data: HashMap<&str, &str>, post_data: Body, headers: HashMap<&str, &str>) -> ResponseFuture {
+    let mut uri = Url::parse(url).unwrap();
+    for (key, value) in get_data.iter() {
+      uri.query_pairs_mut().append_pair(key, value);
+    }
+    let mut request = Request::builder()
+      .method(method)
+      .uri(uri.as_str())
+      .body(post_data)
+      .expect("request builder");
+    request = self.set_header(request, headers);
+    Client::new().request(request)
+  }
+}
+
+fn get_preferred_hrp(network_id: Option<u16>) -> &'static str {
+  match network_id {
+    Some(network_id) => {
+      match NETWORK_ID_TO_HRP.get(&network_id) {
+        Some(hrp) => hrp,
+        None => NETWORK_ID_TO_HRP.get(&DEFAULT_NETWORK_ID).unwrap()
+      }
+    },
+    None => FALLBACK_HRP
   }
 }
 
@@ -39,6 +105,64 @@ impl AvalancheCore for Avalanche {
         }
         self.url = format!("{}://{}:{}", &protocol_defined, &host, &port);
         Ok(())
+    }
+    fn get_protocol(&self) -> &str {
+        &self.protocol
+    }
+    fn get_host(&self) -> &str {
+        &self.host
+    }
+    fn get_ip(&self) -> &str {
+        &self.ip
+    }
+    fn get_port(&self) -> u32 {
+        self.port
+    }
+    fn get_url(&self) -> &str {
+        &self.url
+    }
+    fn get_headers(&self) -> &HashMap<String, String> {
+        &self.headers
+    }
+    fn get_network_id(&self) -> u16 {
+        self.network_id
+    }
+    fn set_network_id(&mut self, network_id: u16) {
+        self.network_id = network_id;
+        self.hrp = String::from(get_preferred_hrp(Some(network_id)));
+    }
+    fn get_hrp(&self) -> &str {
+        &self.hrp
+    }
+    fn set_hrp(&mut self, hrp: &str) {
+        self.hrp = String::from(hrp);
+    }
+    fn set_header(&mut self, key: &str, value: &str) {
+        self.headers.insert(String::from(key), String::from(value));
+    }
+    fn remove_header(&mut self, key: &str) {
+        self.headers.remove(key);
+    }
+    fn remove_all_headers(&mut self) {
+        self.headers.clear();
+    }
+    fn set_auth_token(&mut self, token: &str) {
+        self.auth = Some(String::from(token));
+    }
+    fn get(&self, url: &str, get_data: HashMap<&str, &str>, headers: HashMap<&str, &str>) -> ResponseFuture {
+      self.request(url, Method::GET, get_data, Body::empty(), headers)
+    }
+    fn delete(&self, url: &str, get_data: HashMap<&str, &str>, headers: HashMap<&str, &str>) -> ResponseFuture {
+      self.request(url, Method::DELETE, get_data, Body::empty(), headers)
+    }
+    fn put(&self, url: &str, post_data: Body, headers: HashMap<&str, &str>) -> ResponseFuture {
+      self.request(url, Method::PUT, HashMap::new(), post_data, headers)
+    }
+    fn post(&self, url: &str, post_data: Body, headers: HashMap<&str, &str>) -> ResponseFuture {
+      self.request(url, Method::POST, HashMap::new(), post_data, headers)
+    }
+    fn patch(&self, url: &str, post_data: Body, headers: HashMap<&str, &str>) -> ResponseFuture {
+      self.request(url, Method::PATCH, HashMap::new(), post_data, headers)
     }
 }
 
@@ -100,188 +224,8 @@ mod tests {
     protected headers: { [k: string]: string } = {}
     protected requestConfig: AxiosRequestConfig = {}
     protected apis: { [k: string]: APIBase } = {}
-  
-    /**
-     * Sets the address and port of the main Avalanche Client.
-     *
-     * @param host The hostname to resolve to reach the Avalanche Client RPC APIs.
-     * @param port The port to resolve to reach the Avalanche Client RPC APIs.
-     * @param protocol The protocol string to use before a "://" in a request,
-     * ex: "http", "https", etc. Defaults to http
-     * The following special characters are removed from host and protocol
-     * &#,@+()$~%'":*?{} also less than and greater than signs
-     */
-    setAddress = (
-      host: string,
-      port: number,
-      protocol: string = "http"
-    ): void => {
-      host = host.replace(/[&#,@+()$~%'":*?<>{}]/g, "")
-      protocol = protocol.replace(/[&#,@+()$~%'":*?<>{}]/g, "")
-      const protocols: string[] = ["http", "https"]
-      if (!protocols.includes(protocol)) {
-        /* istanbul ignore next */
-        throw new ProtocolError(
-          "Error - AvalancheCore.setAddress: Invalid protocol"
-        )
-      }
-  
-      this.host = host
-      this.port = port
-      this.protocol = protocol
-      let url: string = `${protocol}://${host}`
-      if (port != undefined && typeof port === "number" && port >= 0) {
-        url = `${url}:${port}`
-      }
-      this.url = url
-    }
-  
-    /**
-     * Returns the protocol such as "http", "https", "git", "ws", etc.
-     */
-    getProtocol = (): string => this.protocol
-  
-    /**
-     * Returns the host for the Avalanche node.
-     */
-    getHost = (): string => this.host
-  
-    /**
-     * Returns the IP for the Avalanche node.
-     */
-    getIP = (): string => this.host
-  
-    /**
-     * Returns the port for the Avalanche node.
-     */
-    getPort = (): number => this.port
-  
-    /**
-     * Returns the URL of the Avalanche node (ip + port)
-     */
-    getURL = (): string => this.url
-  
-    /**
-     * Returns the custom headers
-     */
-    getHeaders = (): object => this.headers
-  
-    /**
-     * Returns the custom request config
-     */
-    getRequestConfig = (): AxiosRequestConfig => this.requestConfig
-  
-    /**
-     * Returns the networkID
-     */
-    getNetworkID = (): number => this.networkID
-  
-    /**
-     * Sets the networkID
-     */
-    setNetworkID = (netID: number): void => {
-      this.networkID = netID
-      this.hrp = getPreferredHRP(this.networkID)
-    }
-  
-    /**
-     * Returns the Human-Readable-Part of the network associated with this key.
-     *
-     * @returns The [[KeyPair]]'s Human-Readable-Part of the network's Bech32 addressing scheme
-     */
-    getHRP = (): string => this.hrp
-  
-    /**
-     * Sets the the Human-Readable-Part of the network associated with this key.
-     *
-     * @param hrp String for the Human-Readable-Part of Bech32 addresses
-     */
-    setHRP = (hrp: string): void => {
-      this.hrp = hrp
-    }
-  
-    /**
-     * Adds a new custom header to be included with all requests.
-     *
-     * @param key Header name
-     * @param value Header value
-     */
-    setHeader = (key: string, value: string): void => {
-      this.headers[`${key}`] = value
-    }
-  
-    /**
-     * Removes a previously added custom header.
-     *
-     * @param key Header name
-     */
-    removeHeader = (key: string): void => {
-      delete this.headers[`${key}`]
-    }
-  
-    /**
-     * Removes all headers.
-     */
-    removeAllHeaders = (): void => {
-      for (const prop in this.headers) {
-        if (Object.prototype.hasOwnProperty.call(this.headers, prop)) {
-          delete this.headers[`${prop}`]
-        }
-      }
-    }
-  
-    /**
-     * Adds a new custom config value to be included with all requests.
-     *
-     * @param key Config name
-     * @param value Config value
-     */
-    setRequestConfig = (key: string, value: string | boolean): void => {
-      this.requestConfig[`${key}`] = value
-    }
-  
-    /**
-     * Removes a previously added request config.
-     *
-     * @param key Header name
-     */
-    removeRequestConfig = (key: string): void => {
-      delete this.requestConfig[`${key}`]
-    }
-  
-    /**
-     * Removes all request configs.
-     */
-    removeAllRequestConfigs = (): void => {
-      for (const prop in this.requestConfig) {
-        if (Object.prototype.hasOwnProperty.call(this.requestConfig, prop)) {
-          delete this.requestConfig[`${prop}`]
-        }
-      }
-    }
-  
-    /**
-     * Sets the temporary auth token used for communicating with the node.
-     *
-     * @param auth A temporary token provided by the node enabling access to the endpoints on the node.
-     */
-    setAuthToken = (auth: string): void => {
-      this.auth = auth
-    }
-  
-    protected _setHeaders = (headers: any): AxiosRequestHeaders => {
-      if (typeof this.headers === "object") {
-        for (const [key, value] of Object.entries(this.headers)) {
-          headers[`${key}`] = value
-        }
-      }
-  
-      if (typeof this.auth === "string") {
-        headers.Authorization = `Bearer ${this.auth}`
-      }
-      return headers
-    }
-  
+
+
     /**
      * Adds an API to the middleware. The API resolves to a registered blockchain's RPC.
      *
